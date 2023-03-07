18807a967991d757fc5654cb537200b7c00593fa
Julia Dlutek <jdlutek987@gmail.com>
2023-03-07 15:03:28 +0100

    task 1 - add mouseover listener

--- task-1/app.js
+++ task-1/app.js
@@ -3,11 +3,6 @@ const URL = 'https://rekrutacja.webdeveloper.rtbhouse.net/files/banner.json';
 let productsContainer = document.querySelector('.products-container');
 let activeElementIdx = 0;
 
-const main = () => {
-  fetchPhotos();
-  setInterval(borderAnimation, 1000);
-};
-
 const fetchPhotos = () => {
   fetch(URL)
     .then((res) => res.json())
@@ -15,12 +10,11 @@ const fetchPhotos = () => {
       const randomProducts = data.offers
         .sort(() => 0.5 - Math.random())
         .slice(0, 4);
-      console.log(randomProducts);
       randomProducts.map((product) => {
         const newProduct = createProductCard(product);
         productsContainer.append(newProduct);
       });
-      borderAnimation();
+      borderAnimationHandler();
     })
     .catch((err) => console.error(err));
 };
@@ -43,14 +37,31 @@ const createProductCard = (product) => {
   return productCard;
 };
 
+const borderAnimationHandler = () => {
+  const allProducts = document.querySelectorAll('.product');
+  let borderInterval = setInterval(borderAnimation, 2000);
+
+  [...allProducts].map((product, idx) => {
+    product.addEventListener('mouseover', () => {
+      clearInterval(borderInterval);
+      allProducts[activeElementIdx].classList.remove('product--active');
+      activeElementIdx = idx;
+      allProducts[activeElementIdx].classList.add('product--active');
+    });
+
+    product.addEventListener('mouseout', () => {
+      borderInterval = setInterval(borderAnimation, 2000);
+    });
+  });
+};
+
 const borderAnimation = () => {
   const allProducts = document.querySelectorAll('.product');
-  allProducts[activeElementIdx].classList.remove('active');
+  allProducts[activeElementIdx].classList.remove('product--active');
   activeElementIdx === allProducts.length - 1
     ? (activeElementIdx = 0)
     : (activeElementIdx += 1);
-  allProducts[activeElementIdx].classList.add('active');
-  console.log(activeElementIdx);
+  allProducts[activeElementIdx].classList.add('product--active');
 };
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener('DOMContentLoaded', fetchPhotos);
